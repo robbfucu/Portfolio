b4cdcea9fd02792af97619edf101f9e8af6a3ecc
Roberto Fucugauchi <robbfucu@gmail.com>
2023-10-21 00:25:24 -0600

    fixes to Home component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,7 @@
 import LogoTitle from '../../assets/images/logo-s.png'
 import { Link } from 'react-router-dom'
+import './index.scss'
+
 const Home = () => {
   return (
     <div className="container home-page">
